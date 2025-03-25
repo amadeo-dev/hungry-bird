4da19b6df4acc461e2eff76f5d81c06877df3fe3
Nicolas <nicolas.yungmann@efrei.net>
2025-03-25 08:43:42 +0100

    zgueg

--- Constantes.py
+++ Constantes.py
@@ -6,9 +6,10 @@ bird_name = ['Jacky', 'Thomas', 'Adrien', 'Nicolas', 'Amadeo' ]
 WIDTH, HEIGHT = 1280, 720
 physique = pymunk.Space()
 physique.gravity = (0,900)
-HOTDOG_IMG = pygame.transform.scale(pygame.image.load("Ressources/image/hotdog.png"), (50, 30))
-BURGER_IMG = pygame.transform.scale(pygame.image.load("Ressources/image/burger.png"), (50, 50))
-BROCOLI_IMG = pygame.transform.scale(pygame.image.load("Ressources/image/brocolis.png"), (40, 40))
-DINDE_IMG = pygame.transform.scale(pygame.image.load("Ressources/image/Dinde Royale.png"), (60, 60))
-RESTART_IMG = pygame.transform.scale(pygame.image.load("Ressources/image/Restart.png"), (50, 50))
-DECORS_IMG = pygame.transform.scale(pygame.image.load("Ressources/image/decor.png"), (WIDTH, HEIGHT))
+
+HOTDOG = pygame.transform.scale(pygame.image.load("Ressources/image/hotdog.png"), (50, 30))
+BURGER = pygame.transform.scale(pygame.image.load("Ressources/image/burger.png"), (50, 50))
+BROCOLI = pygame.transform.scale(pygame.image.load("Ressources/image/brocolis.png"), (40, 40))
+DINDE = pygame.transform.scale(pygame.image.load("Ressources/image/Dinde Royale.png"), (60, 60))
+RESTART = pygame.transform.scale(pygame.image.load("Ressources/image/Restart.png"), (50, 50))
+DECORS = pygame.transform.scale(pygame.image.load("Ressources/image/decor.png"), (WIDTH, HEIGHT))
