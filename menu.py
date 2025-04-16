90fa1f5f5243e4a44b0d889667589efd7094f195
Amadéo Berthelot <78102463+amadeo-dev@users.noreply.github.com>
2025-04-17 00:03:50 +0200

    qq boutons replacés

--- menu.py
+++ menu.py
@@ -7,17 +7,18 @@ pygame.display.set_caption("Bouton avec image")
 
 fond = pygame.transform.scale(fond, (1280, 720))
 
-def create_button(nom, x, y, tx=150, ty=50):
+def create_button(nom, x, y, tx=330, ty=130):
     bouton = pygame.image.load(f"Ressources/image/Menu/{nom}.png")
     return pygame.transform.scale(bouton, (tx, ty)), bouton.get_rect(center=(x, y))
 
 # Boutons
 bouton_Tutoriel, button_rect1 = create_button('Tutoriel', WIDTH // 2, HEIGHT // 2 - 150)
-bouton_niveau1, button_rect2 = create_button('Nv1', WIDTH // 2, HEIGHT // 2 -200)
-bouton_niveau2, button_rect3 = create_button('Nv2', WIDTH // 2, HEIGHT // 2)
-bouton_niveau3, button_rect4 = create_button('Nv3', WIDTH // 2, HEIGHT // 2 +200)
-bouton_quitter, button_rect5 = create_button('Quitter', WIDTH - 100, HEIGHT -50)
-bouton_Reglage, button_rect6 = create_button('Reglages', WIDTH - 100, 50, tx=96, ty=96)
+bouton_niveau1, button_rect2 = create_button('Nv1', 730, 330)
+bouton_niveau2, button_rect3 = create_button('Nv2', 750, 430)
+bouton_niveau3, button_rect4 = create_button('Nv3', 730, 560)
+bouton_quitter, button_rect5 = create_button('Quitter',  100, 560, tx=200, ty=200)
+bouton_Reglage, button_rect6 = create_button('Reglages', 400, 700, tx=250, ty=200)
+bouton_Tutoriel, button_rect7 = create_button('Tutoriel', 700, 900, tx=250, ty=200)
 
 def menu():
     while True:
@@ -28,6 +29,7 @@ def menu():
         screen.blit(bouton_niveau3, button_rect4)
         screen.blit(bouton_quitter, button_rect5)
         screen.blit(bouton_Reglage, button_rect6)
+        screen.blit(bouton_Tutoriel, button_rect7)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -43,5 +45,7 @@ def menu():
                     return "quitter"
                 elif button_rect6.collidepoint(event.pos):
                     return "reglage"
+                #elif button_rect7.collidepoint(event.pos):
+                #    return ("tutoriel")
 
         pygame.display.flip()
