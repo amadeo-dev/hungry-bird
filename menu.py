0c74a4b057ef7e92079a499e4411432ef104c387
Amadéo Berthelot <78102463+amadeo-dev@users.noreply.github.com>
2025-04-18 14:25:23 +0200

    on est bon

--- menu.py
+++ menu.py
@@ -8,22 +8,27 @@ pygame.display.set_caption("Menu")
 fond = pygame.image.load("Ressources/image/Menu/Decors.png")
 fond = pygame.transform.scale(fond, (screen_width, screen_height))
 
+def ajustx(x):
+    return (x*screen_width)/1920
+def ajusty(y):
+     return (y * screen_height) / 1080
+
 def create_button(nom, x, y, tx, ty):
     bouton = pygame.image.load(f"Ressources/image/Menu/{nom}.png")
     return pygame.transform.scale(bouton, (tx, ty)), bouton.get_rect(center=(x, y))
 
 # Boutons
-bouton_Tutoriel, button_rect1 = create_button('Tutoriel', 1400, 930, tx=285, ty=203)
-bouton_niveau1, button_rect2 = create_button('Nv1', 940, 450, tx=520, ty=188)
-bouton_niveau2, button_rect3 = create_button('Nv2', 960, 620, tx=520, ty=188)
-bouton_niveau3, button_rect4 = create_button('Nv3', 940, 790, tx=520, ty=188)
-bouton_quitter, button_rect5 = create_button('Quitter',  150, 700, tx=247, ty=274)
-bouton_Reglage, button_rect6 = create_button('Reglages', 500, 930, tx=325, ty=235)
+bouton_Tutoriel, button_rect1 = create_button('Tutoriel', ajustx(1450), ajusty(980), tx=ajustx(285), ty=ajusty(203))
+bouton_niveau1, button_rect2 = create_button('Nv1', ajustx(1050), ajusty(470), tx=ajustx(520), ty=ajusty(188))
+bouton_niveau2, button_rect3 = create_button('Nv2', ajustx(1080), ajusty(640), tx=ajustx(520), ty=ajusty(188))
+bouton_niveau3, button_rect4 = create_button('Nv3', ajustx(1050), ajusty(810), tx=ajustx(520), ty=ajusty(188))
+bouton_quitter, button_rect5 = create_button('Quitter',  ajustx(200), ajusty(760), tx=ajustx(247), ty=ajusty(247))
+bouton_Reglage, button_rect6 = create_button('Reglages', ajustx(570), ajusty(980), tx=ajustx(325), ty=ajusty(235))
 
 
 def menu():
     while True:
-        screen.blit(fond, (0, 0))
+        screen.blit(fond, (ajustx(0), ajusty(0)))
         screen.blit(bouton_Tutoriel, button_rect1)
         screen.blit(bouton_niveau1, button_rect2)
         screen.blit(bouton_niveau2, button_rect3)
