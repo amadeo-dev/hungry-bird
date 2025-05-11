45eb2d68f66710f956d94b7cce0e8b84d5727373
Nicolas <nicolas.yungmann@efrei.net>
2025-05-11 16:25:21 +0200

    Retour au menu marche, mais on peut pas rechoisir les oiseaux

--- main.py
+++ main.py
@@ -13,21 +13,26 @@ from Reglage import *
 
 
 def main():
-
     while True:
         action = menu()
 
         if action == "niveau1":
-            jeu(1)
+            result = jeu(1)
+            if result == "menu":
+                continue  # Revenir au menu principal
         elif action == "niveau2":
-            jeu(2)
+            result = jeu(2)
+            if result == "menu":
+                continue
         elif action == "niveau3":
-            jeu(3)
+            result = jeu(3)
+            if result == "menu":
+                continue
         elif action == "reglage":
             reglages()
-
         elif action == "quitter":
             break
+
     pygame.mixer.music.stop()
     pygame.quit()
 
